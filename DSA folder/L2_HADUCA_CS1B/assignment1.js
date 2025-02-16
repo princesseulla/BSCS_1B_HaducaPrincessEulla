f0b96f7c1286b20028fd04877ef0688ad86cdffc
princesseulla <phaducaprincesseulla@gmail.com>
2025-02-16 14:59:28 +0800

    Update assignment1.js

--- DSA folder/L2_HADUCA_CS1B/assignment1.js
+++ DSA folder/L2_HADUCA_CS1B/assignment1.js
@@ -2,11 +2,11 @@
 const firstName1 = "Princess";
 const middleName1 = "Pedrozo";
 const lastName1 = "Haduca";
-const birthdate1 = "Julu 12, 2006";
+const birthdate1 = "July 12, 2006";
 const birthplace1 = "Damacuag Sta. Lucia Ilocos Sur, 2712";
 const address1 =  "Damacuag Sta. Lucia Ilocos Sur, 2712";
 const course1 = " Bachelor of Scince in Computer Science";
-const dreamJob1 = " Software Engineer";
+const dreamJob1 = "Software Engineer";
 
 // Student 2 (Classmate)
 const firstName2 = "Lariane";
@@ -15,8 +15,8 @@ const lastName2 = "Arquero";
 const birthdate2 = "November 19, 2005";
 const birthplace2 = "Baluarte, Salcedo Ilocos Sur, 2711";
 const address2 =  "Baluarte, Salcedo Ilocos Sur, 2711";
-const course2 = " Bachelor of Scince in Computer Science";
-const dreamJob2 = " Software Engineering";
+const course2 = "Bachelor of Scince in Computer Science";
+const dreamJob2 = "Software Engineering";
 
 // Student 3(Classmate)
 const firstName3 = "Mariane";
@@ -25,14 +25,14 @@ const lastName3 = "Belen";
 const birthdate3 = "May 23, 2006";
 const birthplace3 = "La Trinidad Benguet, 2601";
 const address3 =  "San Andress, Candon City Ilocos Sur, 2720";
-const course3 = " Bachelor of Scince in Computer Science";
-const dreamJob3 = " Software Engineer";
+const course3 = "Bachelor of Scince in Computer Science";
+const dreamJob3 = "Software Engineer";
 
 // This the function to format and print student details
 function printStudentInfo(firstName,middleName,lastName,birthdate,birthplace,address,course,dreamJob) {
  
 console.log(`[${firstName.toUpperCase()} ${middleName.toUpperCase()} ${lastName.toUpperCase()} was born [${birthdate}] at [${birthplace}], and currently living at [${address}] `);
-console.log(`[${firstName.toLowerCase()} ${middleName.toLowercase()} ${lastName.toLowerCase()}] is taking up [${course}] and dreams to be [${dreamJob}] after graduation. \n`);
+console.log(`[${firstName.toLowerCase()} ${middleName.toLowerCase()} ${lastName.toLowerCase()}] is taking up [${course}] and dreams to be [${dreamJob}] after graduation. \n`);
 }
 
 // Printing details information of students 
