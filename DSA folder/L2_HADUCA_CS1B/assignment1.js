a5e53c6f70555eaf9aa1e9f2885ad2c689321361
princesseulla <phaducaprincesseulla@gmail.com>
2025-02-16 14:48:57 +0800

    Update assignment1.js

--- DSA folder/L2_HADUCA_CS1B/assignment1.js
+++ DSA folder/L2_HADUCA_CS1B/assignment1.js
@@ -18,7 +18,7 @@ const address2 =  "Baluarte, Salcedo Ilocos Sur, 2711";
 const course2 = " Bachelor of Scince in Computer Science";
 const dreamJob2 = " Software Engineering";
 
-// Student 3(Classmate)const firstName1 = "Princess";
+// Student 3(Classmate)
 const firstName3 = "Mariane";
 const middleName3 = "Ingen";
 const lastName3 = "Belen";
@@ -28,14 +28,14 @@ const address3 =  "San Andress, Candon City Ilocos Sur, 2720";
 const course3 = " Bachelor of Scince in Computer Science";
 const dreamJob3 = " Software Engineer";
 
-// This the function to format and print student details.
+// This the function to format and print student details
 function printStudentInfo(firstName,middleName,lastName,birthdate,birthplace,address,course,dreamJob) {
  
 console.log(`[${firstName.toUpperCase()} ${middleName.toUpperCase()} ${lastName.toUpperCase()} was born [${birthdate}] at [${birthplace}], and currently living at [${address}] `);
 console.log(`[${firstName.toLowerCase()} ${middleName.toLowercase()} ${lastName.toLowerCase()}] is taking up [${course}] and dreams to be [${dreamJob}] after graduation. \n`);
 }
 
-// Printing details information of all studentsprintStudentInfo(firstName1, middleName1, lastName1, birthdate1, birthplace1, address1, course1, dreamJob1
+// Printing details information of students 
 printStudentInfo(firstName1, middleName1, lastName1, birthdate1, birthplace1, address1, course1, dreamJob1);
 printStudentInfo(firstName2, middleName2, lastName2, birthdate2, birthplace2, address2, course2, dreamJob2);
 printStudentInfo(firstName3, middleName3, lastName3, birthdate3, birthplace3, address3, course3, dreamJob3);
