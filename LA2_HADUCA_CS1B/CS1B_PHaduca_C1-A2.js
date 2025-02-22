d997ec9f72d7b6c7aac54c0dcde2f0bc4f0151f6
princesseulla <phaducaprincesseulla@gmail.com>
2025-02-22 12:07:12 +0800

    Add files via upload

---
+++ LA2_HADUCA_CS1B/CS1B_PHaduca_C1-A2.js
@@ -0,0 +1,31 @@
+// Declare variable and constants
+let a = 25;
+let b = 20;
+let c = 12;
+let d = "15";
+let e = 15;
+ 
+// Display the sum of all varialbles including its constant
+let sum = a+b+c + Number (d) + e;
+console.log(` sum of all variables: ${sum}`);
+
+// Compare variable d and constant e using relational operators
+console.log(`d > e : ${d > e}`);
+console.log(`d < e : ${d < e}`);
+console.log(`d == e : ${d == e}`);
+console.log(`d === e : ${d === e}`);
+console.log(`d >= e : ${d >= e}`);
+console.log(`d <= e : ${d <= e}`);
+
+// Perform the mathematical operators
+let Subtraction = a-b-c-e;
+console.log(`Subtraction result: ${Subtraction}`);
+
+let Multiplication,Division = (a*c)/e;
+console.log(`Multiplication of a and c, devide by e: ${Multiplication,Division}`);
+ 
+let Exponentiation = e**c;
+console.log(`e raised to power of c: ${Exponentiation}`);
+
+c = 30 
+console.log(`New value of c: ${c}`);
