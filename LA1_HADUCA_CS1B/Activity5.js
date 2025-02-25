a685dfe61c0f4525c6fa6576dea729dc4a2454b7
princesseulla <phaducaprincesseulla@gmail.com>
2025-02-25 18:44:35 +0800

    Add files via upload

---
+++ LA1_HADUCA_CS1B/Activity5.js
@@ -0,0 +1,17 @@
+// Strings
+// I declare my favorite korean actor as my variable name
+let favActorFirstName = "Eon";
+let favActorLastName = "Woo";
+// and concatinate my variable name
+let FullName = favActorFirstName + "" + favActorLastName;
+// and turn my variable name into caoitalize uppercase
+let uppercase = FullName.toUpperCase();
+//and i create a variable name which is message and a message for my favactor
+let message = " My favirite Actor is {uppercase}" ;
+message += " he is the best leading man actor in kdrama." ;
+// and declare my variables
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(FullName);
+console.log(uppercase);
+console.log(message);
